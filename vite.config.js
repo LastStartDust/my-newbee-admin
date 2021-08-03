5a15fa0b89d0fea24dce35b2684050f3d1efefbb
caohai <1106225027@qq.com>
2021-08-03 12:02:00 +0800

    配置目录别名

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      // 项目根目录
+      '~': path.resolve(__dirname, './'),
+      // 项目的src目录
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
