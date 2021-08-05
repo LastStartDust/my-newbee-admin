a5aea6a21734f564c2fe15f746857cc6ae17b3bf
caohai <1106225027@qq.com>
2021-08-05 17:40:57 +0800

    配置element-plus按需引入

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { ElButton } from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 import router from '@/router/index';
 
 // 创建应用实例
 const app = createApp(App)
+app.use(ElButton)
 app.use(router)
 
 // 挂载
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default ({mode}) => {
@@ -9,7 +10,23 @@ export default ({mode}) => {
   const env = { ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      styleImport({
+        libs: [{
+          libraryName: 'element-plus',
+          esModule: true,
+          ensureStyleFile: true,
+          resolveStyle: (name) => {
+            name = name.slice(3)
+            return `element-plus/packages/theme-chalk/src/${name}.scss`;
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`;
+          },
+        }]
+      })
+    ],
     resolve: {
       alias: {
         // 项目根目录
