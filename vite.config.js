948a167b0e0937e3e886d56a06d8d1d867ec7627
caohai <1106225027@qq.com>
2021-08-06 14:26:56 +0800

    优化开发环境请求代理

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default ({mode}) => {
   // 导入环境配置，获取环境变量值
   // import.meta.env.VITE_NAME 相当于这里的: env.VITE_NAME
   const env = { ...loadEnv(mode, process.cwd()) }
+  const { VITE_APP_BASE_API, VITE_APP_DEV_PROXY_TARGET } = env
 
   return defineConfig({
     plugins: [
@@ -37,12 +38,12 @@ export default ({mode}) => {
     },
     server: {
       proxy: {
-        '/dev-api': {
+        [`${VITE_APP_BASE_API}`]: {
           // 凡是遇到 /dev-api 路径的请求，都映射到 target 属性
-          target: env.VITE_APP_DEV_PROXY,
+          target: VITE_APP_DEV_PROXY_TARGET,
           changeOrigin: true,
-          // 重写 /dev-api 为空
-          rewrite: path => path.replace(/^\/dev-api/, '')
+          // 重写 /dev-api 为空, new RegExp(`^${prefix}`)
+          rewrite: path => path.replace(new RegExp(`^${VITE_APP_BASE_API}`), '')
         }
       }
     }
