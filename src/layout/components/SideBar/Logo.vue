323cfdd7c18db6f550165e3fdded5030717a3e7e
“caohai” <1106225027@qq.com>
2021-08-15 16:30:22 +0800

    更新侧边栏Logo组件，调整样式

--- src/layout/components/SideBar/Logo.vue
+++ src/layout/components/SideBar/Logo.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- 左上角logo+简易说明 -->
-  <div class="head">
-    <a href="" class="logo">
+  <div class="logo-container">
+    <router-link class="logo-link" to="/" >
       <img :src="logo" alt="网站logo图片" class="logo-img" />
-    </a>
-    <span class="title">my-newbee-admin</span>
+      <h1 class="title">{{ title }} </h1>
+    </router-link>
   </div>
 </template>
 <script>
@@ -22,3 +22,38 @@ export default defineComponent({
   },
 });
 </script>
+<style lang="scss" scoped>
+
+.logo-container {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  background: #2b2f3a;
+  text-align: center;
+  overflow: hidden;
+
+  & .logo-link {
+    height: 100%;
+    width: 100%;
+
+    & .logo-img {
+      width: 32px;
+      height: 32px;
+      vertical-align: middle;
+      margin-right: 12px;
+    }
+
+    & .title {
+      display: inline-block;
+      margin: 0;
+      color: #fff;
+      font-weight: 600;
+      line-height: 50px;
+      font-size: 14px;
+      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+      vertical-align: middle;
+    }
+  }
+}
+</style>
