ca30d5e3b4ed26f39b84a8aca79952efc4519af4
“caohai” <1106225027@qq.com>
2021-08-15 17:01:07 +0800

    更新Link组件，新增外链判断

--- src/layout/components/SideBar/Link.vue
+++ src/layout/components/SideBar/Link.vue
@@ -7,6 +7,7 @@
 <script>
 // 自定义link，实现点击侧边栏，改变路由=》改变内容
 import { defineComponent, computed, reactive } from "vue";
+import { isExternal } from '@/utils/validate'
 
 export default defineComponent({
   name: "Link",
@@ -17,21 +18,41 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const targetLink = props.to
-    const type = computed(() => {
-      // TODO 判断是否是外部链接，需要使用a标签
-      return "router-link";
-    });
+    const link = props.to
+    const linkIsExternal = computed(() => isExternal(link))
 
-    const linkProps = reactive({
-      to: targetLink,
+    const type = computed(() => {
+      // 判断是否是外部链接，需要使用a标签
+      if(linkIsExternal.value) {
+        return 'a'
+      }
+      return "router-link";
+    })
+
+    const linkProps = computed(() => {
+      if(linkIsExternal.value) {
+        return {
+          href: link,
+          target: '_blank',
+          rel: 'noopener'
+        }
+      }
+      return {
+        to: link
+      }
     })
 
     return {
       type,
       linkProps
-    };
-  },
-});
+    }
+  }
+})
 </script>
-
+<style lang="scss" scoped>
+a {
+  display: inline-block;
+  width: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/utils/validate.js
@@ -0,0 +1,8 @@
+/**
+ * @description 验证是否是外链
+ * @param {string} path
+ * @returns {Boolean}
+ */
+export function isExternal(path) {
+  return /^(https?:|mailto:|tel:)/.test(path)
+}
