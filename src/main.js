453ec32558aa9df35a97c52a7698cf8c45251340
caohai <1106225027@qq.com>
2021-08-18 18:12:21 +0800

    主题修改（暂时无法生效）

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { useElementPlus } from '@/plugins/element-plus';
 import router from '@/router/index';
 import '@/styles/index.scss';
 import svgIcon from '@/components/svgIcon/index.vue'
+// 目前版本不支持主题色修改，待element-plus迁移scss到css完成
+// import '@/styles/element-variables.module.scss'
 
 // 创建应用实例
 const app = createApp(App)
