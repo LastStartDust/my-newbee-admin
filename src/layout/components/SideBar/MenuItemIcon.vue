cd3450ba798833351b71bfd8dfbadbc0512a3a19
caohai <1106225027@qq.com>
2021-08-20 17:32:08 +0800

    新增菜单图标和菜单标题组件

---
+++ src/layout/components/SideBar/MenuItemIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <template v-if="icon">
+    <i v-if="icon.includes('el-icon')" class="sub-el-icon" :class="icon" />
+    <svg-icon v-else :icon-class="icon" />
+  </template>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'MenuItemIcon',
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.sub-el-icon {
+  color: currentColor;
+  width: 1em;
+  height: 1em;
+}
+</style>
---
+++ src/layout/components/SideBar/MenuItemTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <span v-if="title">{{ title }}</span>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'MenuItemTitle',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    }
+  }
+})
+</script>
