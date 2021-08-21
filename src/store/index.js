d304304eee1c333ad75f2f03ad04b28f7db2f92c
caohai <1106225027@qq.com>
2021-08-21 18:29:12 +0800

    新增tagView标签页状态管理

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import getters from './getters'
 import app from './modules/app'
 import permission from './modules/permission'
 import settings from './modules/settings'
+import tagsView from './modules/tagsView'
 
 const modules = {
   app,
   permission,
   settings,
+  tagsView
 }
 
 const store = createStore({
