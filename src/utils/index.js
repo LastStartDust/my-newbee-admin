825aa9d9f73dbb2c82a82ba8bd333011c058793e
caohai <1106225027@qq.com>
2021-08-24 18:28:41 +0800

    新增生成option列表工具函数

--- src/utils/index.js
+++ src/utils/index.js
@@ -15,3 +15,20 @@
  * @return {Object} 返回import.meta.env对象，对象内容有 
  */
 export const env = import.meta.env
+
+/**
+ * @description 生成OptionList
+ * @param { object } status 英文键名和值的映射对象
+ * @param { object } statusMap 状态和中文的映射对象
+ * @returns { array } tempOptionsList 根据status和statusMap，返回一个生成好的新数组
+ * @example [{ value: 0, label: '正常' }, { value: 1, label: '保留' }, { value: 3, label: '删除' }]
+ */
+ export function genOptionList(status, statusMap) {
+  const tempOptionsList = []
+  for (const key in status) {
+    const value = status[key]
+    const label = statusMap[value]
+    tempOptionsList.push({ value, label })
+  }
+  return tempOptionsList
+}
