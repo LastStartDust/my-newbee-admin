9fc1bd76480551d8284543a9c5674cdd561ac822
caohai <1106225027@qq.com>
2021-08-26 16:33:27 +0800

    调整一级菜单折叠时hover显示

--- src/layout/components/SideBar/MenuItemTitle.vue
+++ src/layout/components/SideBar/MenuItemTitle.vue
@@ -1,5 +1,5 @@
 <template>
-  <span v-if="title">{{ title }}</span>
+  <span v-if="title" class="menu-item-title">{{ title }}</span>
 </template>
 <script>
 import { defineComponent } from 'vue'
@@ -9,7 +9,39 @@ export default defineComponent({
     title: {
       type: String,
       default: '',
-    }
-  }
+    },
+  },
 })
 </script>
+<style lang="scss" scoped>
+@import '@/styles/variables.module.scss';
+
+.el-popper.is-dark {
+  & > .menu-item-title {
+    display: inline-block;
+    position: relative;
+    width: 100%;
+    padding: 10px;
+    border-radius: 4px;
+    color: $menuText;
+    background: $menuBg;
+    &:hover {
+      background: $menuHover;
+    }
+
+    &::before {
+      display: block;
+      content: '';
+      box-sizing: border-box;
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translate(-50%, -50%) rotate(45deg);
+      width: 10px;
+      height: 10px;
+      z-index: -1;
+      background-color: $menuBg;
+    }
+  }
+}
+</style>
