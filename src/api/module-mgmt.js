c3fb49e295631725895e34f24d87b07d8ea9b8d1
caohai <1106225027@qq.com>
2021-09-01 16:21:41 +0800

    新增商品接口地址

--- src/api/module-mgmt.js
+++ src/api/module-mgmt.js
@@ -39,3 +39,44 @@ export function deleteCategory(ids) {
     method: 'delete'
   })
 }
+
+// 商品管理
+export function fetchGoodsList(query) {
+  return request({
+    url: '/goods/list',
+    params: query
+  })
+}
+
+export function createGoods(data) {
+  return request({
+    url: '/goods/list',
+    method: 'post',
+    data
+  })
+}
+
+export function updateGoods(data) {
+  return request({
+    url: '/goods/list',
+    method: 'put',
+    data
+  })
+}
+
+export function updateGoodsStatus({ ids, status }) {
+  return request({
+    url: `/goods/status/${status}`,
+    method: 'put',
+    data: {
+      ids,
+    },
+  })
+}
+
+export function fetchGoods(id) {
+  return request({
+    url: `/goods/list/${id}`,
+    method: 'get'
+  })
+}
